86c4ac38bff68fcb07d6f5b912fdc5c8d64f3380
Кирилл Толмачев <stitch00007@gmail.com>
2023-05-04 21:59:47 +0700

    side navigation works

--- project/app/src/main/java/blblblbl/simplelife/weather/MainActivity.kt
+++ project/app/src/main/java/blblblbl/simplelife/weather/MainActivity.kt
@@ -3,14 +3,37 @@ package blblblbl.simplelife.weather
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import blblblbl.simplelife.weather.navigation.AppDestination
+import blblblbl.simplelife.weather.navigation.AppSettingDest
+import blblblbl.simplelife.weather.navigation.AuthorsDest
+import blblblbl.simplelife.weather.navigation.CitiesDest
+import blblblbl.simplelife.weather.navigation.DrawerContent
+import blblblbl.simplelife.weather.navigation.MainDest
+import blblblbl.simplelife.weather.navigation.OnBoardingDest
+import blblblbl.simplelife.weather.navigation.graphs.citiesGraph
 import blblblbl.simplelife.weather.ui.theme.WeatherTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,28 +42,98 @@ class MainActivity : ComponentActivity() {
             WeatherTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    Greeting("Android")
+                    AppScreen(
+                        startDestination = MainDest,
+                        showOnBoarding = false,//(this::showOnBoarding.isInitialized)&&!showOnBoarding.IsShown(),
+                        onBoardingOnClick= {/*showOnBoarding.saveShown()*/}
+
+                    )
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun AppScreen(
+    startDestination: AppDestination = MainDest,
+    showOnBoarding: Boolean = false,
+    onBoardingOnClick:()->Unit = {}
+){
+    val navController = rememberNavController()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val coroutineScope = rememberCoroutineScope()
+    Surface() {
+        ModalNavigationDrawer(
+            drawerContent =
+            {
+                ModalDrawerSheet(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(end = 50.dp)
+                ) {
+                    DrawerContent(
+                        navController = navController,
+                        drawerState = drawerState
+                    )
+                }
 
-@Preview(showBackground = true)
+            },
+            gesturesEnabled = true,
+            drawerState = drawerState
+        ) {
+            AppNavHost(
+                navController = navController,
+                startDestination = startDestination,
+                openMenu = {
+                    coroutineScope.launch {
+                        drawerState.open()
+                    }
+                },
+                showOnBoarding = showOnBoarding,
+                onBoardingOnClick = onBoardingOnClick
+            )
+        }
+    }
+
+}
 @Composable
-fun GreetingPreview() {
-    WeatherTheme {
-        Greeting("Android")
+fun AppNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+    startDestination:AppDestination = MainDest,
+    openMenu: ()->Unit,
+    showOnBoarding: Boolean = false,
+    onBoardingOnClick:()->Unit = {}
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination.route,
+        modifier = modifier
+    ) {
+        composable(route = MainDest.route) {
+            Text(text = MainDest.name)
+        }
+        citiesGraph(navController)
+        composable(route = AppSettingDest.route) {
+            Text(text = AppSettingDest.name)
+        }
+        composable(route = OnBoardingDest.route) {
+            Text(text = OnBoardingDest.name)
+        }
+        composable(route = AuthorsDest.route) {
+            Surface(modifier = Modifier.fillMaxSize()) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(text = "Authors", style = MaterialTheme.typography.headlineLarge)
+                    Text(text = "Kirill Tolmachev", style = MaterialTheme.typography.headlineLarge)
+                    Text(text = "Varvara Sapozhnikova", style = MaterialTheme.typography.headlineLarge)
+                }
+            }
+        }
     }
 }
---
+++ project/app/src/main/java/blblblbl/simplelife/weather/navigation/DrawerContent.kt
@@ -0,0 +1,104 @@
+package blblblbl.simplelife.weather.navigation
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DrawerContent(
+    navController: NavHostController,
+    drawerState: DrawerState
+){
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        Text(text = "pomodoro timer", textAlign = TextAlign.Start, modifier = Modifier.fillMaxWidth())
+        Spacer(modifier = Modifier.height(20.dp))
+        NavigationDrawerItems(
+            navController = navController,
+            drawerState = drawerState
+        )
+        Divider()
+        Spacer(modifier = Modifier.weight(1F))
+
+        Button(onClick = {
+            try {
+                ContextCompat.startActivity(
+                    context,
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("market://details?id=" + context.packageName)
+                    ),
+                    null
+                )
+            } catch (e: ActivityNotFoundException) {
+                ContextCompat.startActivity(
+                    context,
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("http://play.google.com/store/apps/details?id=" + context.packageName)
+                    ),
+                    null
+                )
+            }
+        }) {
+            Text(text = "rate app")
+        }
+    }
+
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NavigationDrawerItems(
+    navController: NavHostController,
+    drawerState: DrawerState
+){
+    var scope = rememberCoroutineScope()
+    var currentBackStackEntryAsState = navController.currentBackStackEntryAsState()
+    var destination = currentBackStackEntryAsState.value?.destination
+
+    Column(modifier = Modifier.fillMaxWidth()) {
+        menuDestinations.forEach {dest->
+            NavigationDrawerItem(
+                label = { Text(text = dest.name) },
+                selected = destination?.route ==dest.route,
+                onClick = {
+                    navController.navigateSingleTopTo(dest.route)
+                    scope.launch {
+                        drawerState.close()
+                    }
+                })
+            Spacer(modifier = Modifier.height(14.dp))
+        }
+    }
+}
