8e7c74999b64786534cb175c58b5118caf76b6d4
blblblblblblblblbl <stitch00007@gmail.com>
2023-05-07 23:06:48 +0700

    add support of custom theme
    add support of custom theme in app layer copied from pomodoro timer

--- project/app/src/main/java/blblblbl/simplelife/weather/MainActivity.kt
+++ project/app/src/main/java/blblblbl/simplelife/weather/MainActivity.kt
@@ -3,6 +3,8 @@ package blblblbl.simplelife.weather
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,7 +17,9 @@ import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -33,17 +37,28 @@ import blblblbl.simplelife.weather.navigation.DrawerContent
 import blblblbl.simplelife.weather.navigation.MainDest
 import blblblbl.simplelife.weather.navigation.OnBoardingDest
 import blblblbl.simplelife.weather.navigation.graphs.citiesGraph
+import blblblbl.simplelife.weather.presentation.MainActivityViewModel
 import blblblbl.simplelife.weather.ui.theme.WeatherTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainActivityViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WeatherTheme {
-                // A surface container using the 'background' color from the theme
+            val systemUiController = rememberSystemUiController()
+            WeatherTheme(
+                configFlow = viewModel.getSettingsFlow()
+            ) {
+                val useDarkIcons = !isSystemInDarkTheme()
+                val color = MaterialTheme.colorScheme.surfaceColorAtElevation(5.dp)
+                SideEffect {
+                    systemUiController.setSystemBarsColor(color, darkIcons = useDarkIcons)
+                }
+
                 Surface(
                     modifier = Modifier.fillMaxSize()
                 ) {
--- project/app/src/main/java/blblblbl/simplelife/weather/ui/theme/Type.kt
+++ project/app/src/main/java/blblblbl/simplelife/weather/ui/theme/Type.kt
@@ -6,29 +6,36 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+val WeatherTypography = Typography(
+
+    headlineLarge = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    headlineMedium = TextStyle(
+        fontWeight = FontWeight.W600,
+        fontSize = 24.sp
+    ),
+    headlineSmall = TextStyle(
+        fontWeight = FontWeight.W600,
+        fontSize = 20.sp
+    ),
+    labelLarge = TextStyle(
+        fontWeight = FontWeight.W600,
+        fontSize = 16.sp
+    ),
+    labelMedium = TextStyle(
+        fontSize = 16.sp
+    ),
+    bodyLarge = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp
+    ),
+    bodyMedium = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    bodySmall = TextStyle(
+        fontSize = 10.sp
     )
-    */
 )
