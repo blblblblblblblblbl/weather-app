1534319004af913802e6790f853bad02e48e311c
Кирилл Толмачев <stitch00007@gmail.com>
2023-05-17 13:26:59 +0700

    widget different sizes

--- project/feature/widget/src/main/java/blblblbl/simplelife/widget/WeatherWidget.kt
+++ project/feature/widget/src/main/java/blblblbl/simplelife/widget/WeatherWidget.kt
@@ -1,17 +1,22 @@
 package blblblbl.simplelife.widget
 
 import android.content.Context
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.datastore.preferences.core.Preferences
 import androidx.glance.GlanceId
+import androidx.glance.LocalSize
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetManager
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
+import androidx.glance.appwidget.SizeMode
 import androidx.glance.appwidget.provideContent
 import androidx.glance.appwidget.state.updateAppWidgetState
 import androidx.glance.appwidget.updateIf
 import androidx.glance.currentState
 import androidx.glance.state.GlanceStateDefinition
 import androidx.glance.state.PreferencesGlanceStateDefinition
+import androidx.glance.text.Text
 import blblblbl.simplelife.forecast.domain.model.forecast.ForecastResponse
 import blblblbl.simplelife.widget.WidgetKeys.Prefs.cityNamePK
 import blblblbl.simplelife.widget.WidgetKeys.Prefs.forecastJSONPK
@@ -19,12 +24,30 @@ import com.google.gson.GsonBuilder
 
 class WeatherWidget :GlanceAppWidget(){
 
+    companion object {
+        private val thinMode = DpSize(120.dp, 120.dp)
+        private val smallMode = DpSize(184.dp, 184.dp)
+        private val mediumMode = DpSize(260.dp, 200.dp)
+        private val largeMode = DpSize(260.dp, 280.dp)
+    }
+
     override var stateDefinition: GlanceStateDefinition<*> = PreferencesGlanceStateDefinition
+
+    override val sizeMode: SizeMode = SizeMode.Responsive(
+        setOf(thinMode, smallMode, mediumMode, largeMode)
+    )
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         provideContent {
             val prefs = currentState<Preferences>()
-            WeatherWidgetContent(prefs)
+            val size = LocalSize.current
+            when (size){
+                thinMode-> WeatherWidgetContentThin(prefs = prefs)
+                smallMode-> WeatherWidgetContentSmall(prefs = prefs)
+                mediumMode-> WeatherWidgetContentMedium(prefs = prefs)
+                largeMode-> WeatherWidgetContentLarge(prefs = prefs)
+            }
+
         }
     }
 
--- project/feature/widget/src/main/java/blblblbl/simplelife/widget/WeatherWidgetContent.kt
+++ project/feature/widget/src/main/java/blblblbl/simplelife/widget/WeatherWidgetContent.kt
@@ -1,12 +1,12 @@
 package blblblbl.simplelife.widget
 
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 import androidx.datastore.preferences.core.Preferences
 import androidx.glance.GlanceModifier
 import androidx.glance.ImageProvider
 import androidx.glance.appwidget.appWidgetBackground
-import androidx.glance.appwidget.lazy.LazyColumn
 import androidx.glance.background
 import androidx.glance.layout.Column
 import androidx.glance.layout.padding
@@ -17,8 +17,8 @@ import blblblbl.simplelife.widget.WidgetKeys.Prefs.forecastJSONPK
 import com.google.gson.GsonBuilder
 
 @Composable
-fun WeatherWidgetContent(prefs:Preferences){
-    val gson = GsonBuilder().setLenient().create()
+fun WeatherWidgetContentThin(prefs:Preferences){
+    /*val gson = GsonBuilder().setLenient().create()
     val cityName = prefs[cityNamePK]?:"city"
     val forecast = gson.fromJson(prefs[forecastJSONPK].orEmpty(),ForecastResponse::class.java)
     Column(
@@ -27,8 +27,64 @@ fun WeatherWidgetContent(prefs:Preferences){
             .appWidgetBackground()
             .padding(16.dp)
     ) {
+
         Text(text = cityName)
         Text(text = forecast.toString())
-    }
+    }*/
+    Text(text = "thin")
 
+}
+@Composable
+fun WeatherWidgetContentSmall(prefs:Preferences){
+    /*val gson = GsonBuilder().setLenient().create()
+    val cityName = prefs[cityNamePK]?:"city"
+    val forecast = gson.fromJson(prefs[forecastJSONPK].orEmpty(),ForecastResponse::class.java)
+    Column(
+        modifier = GlanceModifier
+            .background(imageProvider = ImageProvider(R.drawable.widget_background))
+            .appWidgetBackground()
+            .padding(16.dp)
+    ) {
+
+        Text(text = cityName)
+        Text(text = forecast.toString())
+    }*/
+    Text(text = "small")
+
+}
+
+@Composable
+fun WeatherWidgetContentMedium(prefs:Preferences){
+    /*val gson = GsonBuilder().setLenient().create()
+    val cityName = prefs[cityNamePK]?:"city"
+    val forecast = gson.fromJson(prefs[forecastJSONPK].orEmpty(),ForecastResponse::class.java)
+    Column(
+        modifier = GlanceModifier
+            .background(imageProvider = ImageProvider(R.drawable.widget_background))
+            .appWidgetBackground()
+            .padding(16.dp)
+    ) {
+
+        Text(text = cityName)
+        Text(text = forecast.toString())
+    }*/
+    Text(text = "medium")
+}
+
+@Composable
+fun WeatherWidgetContentLarge(prefs:Preferences){
+    /*val gson = GsonBuilder().setLenient().create()
+    val cityName = prefs[cityNamePK]?:"city"
+    val forecast = gson.fromJson(prefs[forecastJSONPK].orEmpty(),ForecastResponse::class.java)
+    Column(
+        modifier = GlanceModifier
+            .background(imageProvider = ImageProvider(R.drawable.widget_background))
+            .appWidgetBackground()
+            .padding(16.dp)
+    ) {
+
+        Text(text = cityName)
+        Text(text = forecast.toString())
+    }*/
+    Text(text = "large")
 }
