3380362c9c0107afd776a6bdb871c02f3805fb82
Кирилл Толмачев <stitch00007@gmail.com>
2023-08-04 19:33:46 +0700

    add Core and CoreUtils modules

---
+++ project/shared/Core/src/main/java/blblblbl/simplelife/core/presentation/base/BaseViewModel.kt
@@ -0,0 +1,8 @@
+package blblblbl.simplelife.core.presentation.base
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * @author Kirill Tolmachev 04.08.2023
+ */
+abstract class BaseViewModel : ViewModel()
